084b60ad7a7c40ef8b716e417374152f6db6b332
mikemmorgan-dot <mikemmorgan@gmail.com>
2026-06-11 09:04:25 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -365,7 +365,7 @@ app.get("/sw.js", (_, res) => {
   res.set("Content-Type", "application/javascript");
   res.set("Cache-Control", "no-cache");
   res.send(`
-const CACHE = "morgans-kitchen-v43";
+const CACHE = "morgans-kitchen-v45";
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.add("/")));
